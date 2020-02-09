777095b9123464952031c877742f5b94c7eba810
AndrewBenton1 <abenton4416@gmail.com>
2020-02-09 16:25:22 -0500

    Testing changes

--- Tailspin.SpaceGame.Web/Controllers/HomeController.cs
+++ Tailspin.SpaceGame.Web/Controllers/HomeController.cs
@@ -42,7 +42,7 @@ namespace TailSpin.SpaceGame.Web.Controllers
 
                 GameModes = new List<string>()
                 {
-                    "Solo",
+                    "Solo123",
                     "Duo",
                     "Trio"
                 },
